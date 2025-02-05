af5d23175850d6431a125579a4ab0a298b4e1a61
Dmitrii Starkov <starkoffbusiness@gmail.com>
2025-02-05 14:52:01 +0500

    feat: подключил призму

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import { prisma } from '@/shared/lib/db';
 import { Button } from '@/shared/ui/button';
+import { Card, CardTitle } from '@/shared/ui/card';
 
-export default function Home() {
-	return <Button>Click me</Button>;
+export default async function Home() {
+	const games = await prisma.game.findMany();
+
+	return (
+		<div>
+			<Button>Click me</Button>
+			{games.map(game => (
+				<Card key={game.id}>
+					<CardTitle>{game.name}</CardTitle>
+				</Card>
+			))}
+		</div>
+	);
 }
